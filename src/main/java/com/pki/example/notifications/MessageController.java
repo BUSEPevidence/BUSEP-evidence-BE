7cbc2928ce38c06d514c331181b08de5fb2ee52f
mihajlo182savic <savic.ra182.2019@uns.ac.rs>
2023-06-14 20:14:06 +0200

    socketDone

---
+++ src/main/java/com/pki/example/notifications/MessageController.java
@@ -0,0 +1,22 @@
+package com.pki.example.notifications;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@CrossOrigin(maxAge = 3600)
+public class MessageController {
+
+
+    @SendTo("/topic/notification")
+    public String sendNotification(String notification){
+        return notification;
+    }
+
+
+
+}
